d8c5ba543c479b42c02af9d6d8ee467fd82998cb
zyng2003-byte <zyng2003@gmail.com>
2026-04-23 22:48:33 +0800

    ZhenYik

--- src/main/groovy/adamatti/repo/OrderRepo.groovy
+++ src/main/groovy/adamatti/repo/OrderRepo.groovy
@@ -13,65 +13,26 @@ class OrderRepo {
 	@Autowired
 	private MongoTemplate mongoTemplate
 
-	/**
-	 * List orders with optional criteria and pagination
-	 */
-	List list(Map mapCriteria = [:], int skip = 0, int limit = 0) {
+	List list(Map mapCriteria = [:]) {
 		Criteria criteria = toCriteria(mapCriteria)
-		Query query = new Query(criteria)
-		
-		if (skip > 0) query.skip(skip)
-		if (limit > 0) query.limit(limit)
-		
-		mongoTemplate.find(query, Map.class, COLLECTION_NAME)
-	}
-
-	/**
-	 * Get single order by ID
-	 */
-	Map findById(String id) {
-		mongoTemplate.findById(id, Map.class, COLLECTION_NAME)
-	}
-
-	/**
-	 * Count orders matching criteria
-	 */
-	long count(Map mapCriteria = [:]) {
-		Criteria criteria = toCriteria(mapCriteria)
-		Query query = new Query(criteria)
-		mongoTemplate.count(query, COLLECTION_NAME)
+		list(criteria)
 	}
 
 	private Criteria toCriteria(Map map) {
-		if (map.isEmpty()) {
-			return new Criteria()
-		}
-		
-		// Build criteria using fold to avoid reassignment
-		map.inject(new Criteria()) { criteria, k, v ->
-			criteria.and(k).is(v)
-		}
+		map.inject(new Criteria()) { criteria, k, v -> criteria.and(k).is(v) }
 	}
 
-	Map save(Map entity) {
-		mongoTemplate.save(entity, COLLECTION_NAME)
-		entity
-	}
-
-	/**
-	 * Batch insert multiple orders for better performance
-	 */
-	List saveBatch(List<Map> entities) {
-		mongoTemplate.insert(entities, COLLECTION_NAME)
-		entities
-	}
-
-	/**
-	 * Delete orders matching criteria
-	 */
-	long deleteByQuery(Map mapCriteria) {
-		Criteria criteria = toCriteria(mapCriteria)
+	List list(Criteria criteria){
 		Query query = new Query(criteria)
-		mongoTemplate.remove(query, COLLECTION_NAME).deletedCount
+		list(query)
+	}
+
+	List list(Query query){
+		mongoTemplate.find(query,Map.class,COLLECTION_NAME)
+	}
+
+	Map save(Map entity){
+		mongoTemplate.save(entity,COLLECTION_NAME)
+		entity
 	}
 }
